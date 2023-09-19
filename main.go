a6d9ae5f0d4150647d188974a185a98c290b59e2
Dante Trisciuzzi <dante.trisciuzzi@snhu.edu>
2023-09-18 21:06:22 -0400

    add structs

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+)
+
+type HSL struct {
+	H, S, L int
+}
+
+func (hsl *HSL) String() string {
+	return fmt.Sprintf("HSL(%d, %d%%, %d%%)", hsl.H, hsl.S, hsl.L)
+}
+
+type HSV struct {
+	H, S, V int
+}
+
+func (hsl *HSV) String() string {
+	return fmt.Sprintf("HSV(%d, %d%%, %d%%)", hsl.H, hsl.S, hsl.V)
+}
+
+type RGB struct {
+	R, G, B uint8
+}
+
 func main() {
 
 }
