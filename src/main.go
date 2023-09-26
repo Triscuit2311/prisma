fb2d26a411433be34c93204f4e2ed399c0cb59b1
Dante Trisciuzzi <dante.trisciuzzi@snhu.edu>
2023-09-25 20:23:13 -0400

    fix terminal color layout

--- src/main.go
+++ src/main.go
@@ -10,55 +10,51 @@ import (
 func main() {
 	baseColor := ct.NewColorProfileFromRGB(200, 33, 217)
 
-	// Gruvbox color scheme
-	gruvboxColors := NewTerminalPalette(
-		ct.NewColorProfileFromRGB(40, 40, 40),    //Black
-		ct.NewColorProfileFromRGB(204, 36, 29),   //LightRed
-		ct.NewColorProfileFromRGB(152, 151, 26),  //LightGreen
-		ct.NewColorProfileFromRGB(215, 153, 33),  //Yellow
-		ct.NewColorProfileFromRGB(69, 133, 136),  //LightBlue
-		ct.NewColorProfileFromRGB(177, 98, 134),  //LightMagenta
-		ct.NewColorProfileFromRGB(104, 157, 106), //LightCyan
-		ct.NewColorProfileFromRGB(168, 153, 132), //HighWhite
-		ct.NewColorProfileFromRGB(146, 131, 116), //Grey
-		ct.NewColorProfileFromRGB(251, 73, 52),   //Red
-		ct.NewColorProfileFromRGB(184, 187, 38),  //Green
-		ct.NewColorProfileFromRGB(250, 189, 47),  //Brown
-		ct.NewColorProfileFromRGB(131, 165, 152), //Blue
-		ct.NewColorProfileFromRGB(211, 134, 155), //Magenta
-		ct.NewColorProfileFromRGB(142, 192, 124), //Cyan
-		ct.NewColorProfileFromRGB(235, 219, 178), //White
-		"Gruvbox - ",
+	// ubuntuColors := NewTerminalPalette(
+	// 	ct.NewColorProfileFromRGB(0, 0, 0),       //Black
+	// 	ct.NewColorProfileFromRGB(187, 0, 0),     //Red
+	// 	ct.NewColorProfileFromRGB(0, 187, 0),     //Green
+	// 	ct.NewColorProfileFromRGB(187, 187, 0),   //Yellow
+	// 	ct.NewColorProfileFromRGB(0, 0, 187),     //Blue
+	// 	ct.NewColorProfileFromRGB(187, 0, 187),   //Magenta
+	// 	ct.NewColorProfileFromRGB(0, 187, 187),   //Cyan
+	// 	ct.NewColorProfileFromRGB(187, 187, 187), //White
+	// 	ct.NewColorProfileFromRGB(85, 85, 85),    //Grey
+	// 	ct.NewColorProfileFromRGB(255, 85, 85),   //LightRed
+	// 	ct.NewColorProfileFromRGB(85, 255, 85),   //LightGreen
+	// 	ct.NewColorProfileFromRGB(255, 255, 85),  //Brown
+	// 	ct.NewColorProfileFromRGB(85, 85, 255),   //LightBlue
+	// 	ct.NewColorProfileFromRGB(255, 85, 255),  //LightMagenta
+	// 	ct.NewColorProfileFromRGB(85, 255, 255),  //LightCyan
+	// 	ct.NewColorProfileFromRGB(255, 255, 255), //White
+	// 	"(Ubuntu) ",
+	// )
+
+	// Ayu Mirage color scheme
+	ayuMirageColors := NewTerminalPalette(
+		ct.NewColorProfileFromRGB(20, 21, 25),    //Black
+		ct.NewColorProfileFromRGB(255, 85, 85),   //Red
+		ct.NewColorProfileFromRGB(136, 192, 208), //Green
+		ct.NewColorProfileFromRGB(250, 189, 47),  //Yellow
+		ct.NewColorProfileFromRGB(108, 135, 255), //Blue
+		ct.NewColorProfileFromRGB(220, 140, 255), //Magenta
+		ct.NewColorProfileFromRGB(167, 212, 159), //Cyan
+		ct.NewColorProfileFromRGB(173, 178, 183), //White
+		ct.NewColorProfileFromRGB(99, 100, 101),  //Grey
+		ct.NewColorProfileFromRGB(255, 85, 85),   //LightRed
+		ct.NewColorProfileFromRGB(162, 210, 142), //LightGreen
+		ct.NewColorProfileFromRGB(219, 186, 115), //Brown
+		ct.NewColorProfileFromRGB(79, 121, 200),  //LightBlue
+		ct.NewColorProfileFromRGB(197, 120, 228), //LightMagenta
+		ct.NewColorProfileFromRGB(141, 210, 138), //LightCyan
+		ct.NewColorProfileFromRGB(255, 255, 255), //White
+		"Ayu Mirage - ",
 	)
 
-	// Solarized Dark color scheme
-	solarizedDarkColors := NewTerminalPalette(
-		ct.NewColorProfileFromRGB(7, 54, 66),     //Black
-		ct.NewColorProfileFromRGB(220, 50, 47),   //LightRed
-		ct.NewColorProfileFromRGB(133, 153, 0),   //LightGreen
-		ct.NewColorProfileFromRGB(181, 137, 0),   //Yellow
-		ct.NewColorProfileFromRGB(38, 139, 210),  //LightBlue
-		ct.NewColorProfileFromRGB(211, 54, 130),  //LightMagenta
-		ct.NewColorProfileFromRGB(42, 161, 152),  //LightCyan
-		ct.NewColorProfileFromRGB(238, 232, 213), //HighWhite
-		ct.NewColorProfileFromRGB(0, 43, 54),     //Grey
-		ct.NewColorProfileFromRGB(203, 75, 22),   //Red
-		ct.NewColorProfileFromRGB(88, 110, 117),  //Green
-		ct.NewColorProfileFromRGB(101, 123, 131), //Brown
-		ct.NewColorProfileFromRGB(131, 148, 150), //Blue
-		ct.NewColorProfileFromRGB(108, 113, 196), //Magenta
-		ct.NewColorProfileFromRGB(147, 161, 161), //Cyan
-		ct.NewColorProfileFromRGB(253, 246, 227), //White
-		"(Solarized Dark) ",
+	myPalette := GenerateTerminalPalette(
+		baseColor, ayuMirageColors.Magenta, 0.4,
+		ayuMirageColors, "MyPalette", 10, 90, 340, 40, 20,
 	)
-
-	// Closest absolute
-	//fmt.Println(ct.GetClosestColor(&red, ct.GetHarmonics(&baseColor, 20)).RGB.AsHEXSTR())
-
-	diff := ct.TotalDeviance(&gruvboxColors.Magenta.RGB, &baseColor.RGB)
-
-	// Closest SD
-	fmt.Println(ct.GetClosestColorRelative(diff, 0.5, &gruvboxColors.Red, ct.GetHarmonics(&baseColor, 20)).RGB.AsHEXSTR())
 
 	triads := ct.GetHarmonics(&baseColor, 3)
 
@@ -72,8 +68,8 @@ func main() {
 
 	allColorGroups := []displayColorGroup{
 		makeColorGroup([]ct.ColorProfile{baseColor}, "Base Color", "Base "),
-		makeColorGroup(gruvboxColors.asSlice(), "Gruvbox", "gruv"),
-		makeColorGroup(solarizedDarkColors.asSlice(), "Solarized Dark", "solar"),
+		makeColorGroup(ayuMirageColors.asSlice(), "Ayu", "ubu"),
+		makeColorGroup(myPalette.asSlice(), "My Palette", "mine"),
 		makeColorGroup(triads, "Triads - 3 colors evenly distributed", "Triad"),
 		makeColorGroup(tetrads, "Tetrads - 4 colors evenly distributed", "Tetrad"),
 		makeColorGroup(harmonics, "Harmonic Set", "harmonics"),
@@ -96,85 +92,137 @@ func main() {
 	http.ListenAndServe(":9000", nil)
 }
 
+func GenerateTerminalPalette(
+	color, referenceColor ct.ColorProfile, weight float64,
+	referencePalette terminalPalette, name string,
+	monoLightOffsetPercent, monoSatOffsetPercent,
+	monoDegrees, analogousDegrees, colorDensity int) terminalPalette {
+
+	diff := ct.TotalDeviance(&color.RGB, &referenceColor.RGB)
+	harmonics := ct.GetHarmonics(&color, colorDensity)
+	monochromatics := ct.GetMonochromatic(&color, colorDensity, monoDegrees)
+	analogousSet := ct.GetAnalogous(&color, colorDensity, analogousDegrees)
+
+	colorLibrary := append(harmonics, monochromatics...)
+	colorLibrary = append(colorLibrary, analogousSet...)
+
+	return NewTerminalPalette(
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Black, colorLibrary).Darkened(monoLightOffsetPercent).Desaturated(monoSatOffsetPercent), //Black
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Red, colorLibrary), //Red
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Green, colorLibrary), //Green
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Yellow, colorLibrary), //Yellow
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Blue, colorLibrary), //Blue
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Magenta, colorLibrary), //Magenta
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Cyan, colorLibrary), //Cyan
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.White, colorLibrary).Darkened(monoLightOffsetPercent).Desaturated(monoSatOffsetPercent), //White
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Grey, colorLibrary).Lightened(monoLightOffsetPercent).Desaturated(monoSatOffsetPercent), //Grey
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.LightRed, colorLibrary), //LightRed
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.LightGreen, colorLibrary), //LightGreen
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.Brown, colorLibrary), //Brown
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.LightBlue, colorLibrary), //LightBlue
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.LightMagenta, colorLibrary), //LightMagenta
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.LightCyan, colorLibrary), //LightCyan
+		ct.GetClosestColorRelative(diff, weight,
+			&referencePalette.HighWhite, colorLibrary).Lightened(monoLightOffsetPercent).Desaturated(monoSatOffsetPercent), //HighWhite
+		name+" - ",
+	)
+
+}
+
 type terminalPalette struct {
 	Black,
-	LightRed,
-	LightGreen,
-	Yellow,
-	LightBlue,
-	LightMagenta,
-	LightCyan,
-	HighWhite,
-	Grey,
 	Red,
 	Green,
-	Brown,
+	Yellow,
 	Blue,
 	Magenta,
 	Cyan,
-	White ct.ColorProfile
+	White,
+	Grey,
+	LightRed,
+	LightGreen,
+	Brown,
+	LightBlue,
+	LightMagenta,
+	LightCyan,
+	HighWhite ct.ColorProfile
 }
 
 func NewTerminalPalette(
-	Black, LightRed, LightGreen, Yellow, LightBlue,
-	LightMagenta, LightCyan, HighWhite, Grey, Red, Green, Brown,
-	Blue, Magenta, Cyan, White ct.ColorProfile, prefix string) terminalPalette {
+	Black, Red, Green, Yellow, Blue,
+	Magenta, Cyan, White, Grey, LightRed, LightGreen, Brown,
+	LightBlue, LightMagenta, LightCyan, HighWhite ct.ColorProfile, prefix string) terminalPalette {
 
 	Black.SetName(prefix + "Black")
-	LightRed.SetName(prefix + "LightRed")
-	LightGreen.SetName(prefix + "LightGreen")
-	Yellow.SetName(prefix + "Yellow")
-	LightBlue.SetName(prefix + "LightBlue")
-	LightMagenta.SetName(prefix + "LightMagenta")
-	LightCyan.SetName(prefix + "LightCyan")
-	HighWhite.SetName(prefix + "HighWhite")
-	Grey.SetName(prefix + "Grey")
 	Red.SetName(prefix + "Red")
 	Green.SetName(prefix + "Green")
-	Brown.SetName(prefix + "Brown")
+	Yellow.SetName(prefix + "Yellow")
 	Blue.SetName(prefix + "Blue")
 	Magenta.SetName(prefix + "Magenta")
 	Cyan.SetName(prefix + "Cyan")
 	White.SetName(prefix + "White")
+	Grey.SetName(prefix + "Grey")
+	LightRed.SetName(prefix + "LightRed")
+	LightGreen.SetName(prefix + "LightGreen")
+	Brown.SetName(prefix + "Brown")
+	LightBlue.SetName(prefix + "LightBlue")
+	LightMagenta.SetName(prefix + "LightMagenta")
+	LightCyan.SetName(prefix + "LightCyan")
+	HighWhite.SetName(prefix + "HighWhite")
 
 	return terminalPalette{
 		Black,
-		LightRed,
-		LightGreen,
-		Yellow,
-		LightBlue,
-		LightMagenta,
-		LightCyan,
-		HighWhite,
-		Grey,
 		Red,
 		Green,
-		Brown,
+		Yellow,
 		Blue,
 		Magenta,
 		Cyan,
 		White,
+		Grey,
+		LightRed,
+		LightGreen,
+		Brown,
+		LightBlue,
+		LightMagenta,
+		LightCyan,
+		HighWhite,
 	}
 }
 
 func (palette *terminalPalette) asSlice() []ct.ColorProfile {
 	return []ct.ColorProfile{
 		palette.Black,
-		palette.LightRed,
-		palette.LightGreen,
-		palette.Yellow,
-		palette.LightBlue,
-		palette.LightMagenta,
-		palette.LightCyan,
-		palette.HighWhite,
-		palette.Grey,
 		palette.Red,
 		palette.Green,
-		palette.Brown,
+		palette.Yellow,
 		palette.Blue,
 		palette.Magenta,
 		palette.Cyan,
 		palette.White,
+		palette.Grey,
+		palette.LightRed,
+		palette.LightGreen,
+		palette.Brown,
+		palette.LightBlue,
+		palette.LightMagenta,
+		palette.LightCyan,
+		palette.HighWhite,
 	}
 }
 
